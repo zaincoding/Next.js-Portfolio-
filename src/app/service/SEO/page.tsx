252398387df91f8addc2d5fb8a9c6b5a67a00340
Zain Khan <105432110+zaincoding@users.noreply.github.com>
2024-10-14 21:09:40 +0500

    Update page.tsx

--- src/app/service/SEO/page.tsx
+++ src/app/service/SEO/page.tsx
@@ -25,9 +25,9 @@ function SEO(){
           </div>
           <div>
           <h1 className='homePage'>SEO Optimization</h1>
-        <p className={web.p}>I provide professional SEO optimization services to help improve your website's visibility and ranking on search engines like Google. 
+        <p className={web.p}>I provide professional SEO optimization services to help improve your website&apos;s visibility and ranking on search engines like Google. 
             With a focus on both on-page and off-page SEO, I ensure that your site is optimized for relevant keywords, has fast loading times, and is mobile-friendly. 
-            I also implement strategies to enhance your site's content, structure, and backlink profile, driving more organic traffic and improving search rankings.
+            I also implement strategies to enhance your site&apos;s content, structure, and backlink profile, driving more organic traffic and improving search rankings.
              Whether you’re a small business or a growing enterprise, my SEO services are tailored to meet your needs and help you achieve your digital marketing goals.
              </p>
              

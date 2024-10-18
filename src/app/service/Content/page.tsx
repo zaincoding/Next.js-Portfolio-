159dfac62eecd12ca77959f6d3fc8df969f826c6
Zain Khan <105432110+zaincoding@users.noreply.github.com>
2024-10-18 12:20:51 +0500

    Update page.tsx

--- src/app/service/Content/page.tsx
+++ src/app/service/Content/page.tsx
@@ -30,11 +30,14 @@ function ContentWritting(){
            My services focus on creating compelling, SEO-friendly content that improves your search rankings and attracts organic traffic. 
            From engaging readers to building credibility, I ensure that your content resonates with your target audience. Whether you need content for digital marketing, 
            social media, or email campaigns, I deliver impactful writing that helps your business stand out.
-             </p>
+
+                       <div className={web.div}>
+             <button  className={web.btn}><Link href="/contact">Contact Us to Avail Service</Link></button>
+           </div>
+        
+        </p>
              
         </div>
-        <br/> <span className={web.text}>For Contact</span>
-        <button  className={web.btn}><Link href="/contact">Contact Here</Link></button>
         </div>
     )
 }
